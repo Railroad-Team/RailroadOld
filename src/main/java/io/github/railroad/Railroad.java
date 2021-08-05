b638e8a7c3001eb372d79ac497ebc6a0ef161a11
SerialSniper <51060730+SerialSniper@users.noreply.github.com>
2021-08-05 12:51:02 +0200

    Fixed blank select project window, the crash occurring whan closing it and restored discord integration (#10)

--- src/main/java/io/github/railroad/Railroad.java
+++ src/main/java/io/github/railroad/Railroad.java
@@ -14,7 +14,7 @@ import net.arikia.dev.drpc.DiscordRichPresence;
 public class Railroad extends Application {
 
 	public DiscordEventHandlers discordHandlers;
-	public DiscordRichPresence discordRichPresense;
+	public DiscordRichPresence discordRichPresence;
 	private Setup setup;
 
 	/**
@@ -27,21 +27,22 @@ public class Railroad extends Application {
 				.setReadyEventHandler(user -> System.out.println(user.username + "#" + user.discriminator)).build();
 		DiscordRPC.discordInitialize("853387211897700394", this.discordHandlers, true);
 		DiscordRPC.discordRunCallbacks();
-		this.discordRichPresense = new DiscordRichPresence.Builder("Working on Untitled Project")
+		this.discordRichPresence = new DiscordRichPresence.Builder("Working on Untitled Project")
 				.setDetails("Making an amazing mod!").setBigImage("logo", "Railroad IDE")
 				.setSmallImage("logo", "An IDE built for modders, made by modders.").setParty("", 0, 0)
 				.setStartTimestamps(System.currentTimeMillis()).build();
-		DiscordRPC.discordUpdatePresence(this.discordRichPresense);
+		DiscordRPC.discordUpdatePresence(this.discordRichPresence);
 	}
 
 	@Override
 	public void start(final Stage primaryStage) throws Exception {
 		this.setup = new Setup(true);
 
+		setupDiscord();
+
 		final var scene = new Scene(this.setup.mainPane);
 		scene.getStylesheets().add(Railroad.class.getResource("/default.css").toExternalForm());
 		primaryStage.setScene(scene);
-		primaryStage.show();
 		primaryStage.setTitle("Railroad IDE");
 		primaryStage.setWidth(this.setup.primaryScreenBounds.getWidth());
 		primaryStage.setHeight(this.setup.primaryScreenBounds.getHeight());
@@ -50,6 +51,7 @@ public class Railroad extends Application {
 			event.consume();
 			WindowTools.displayQuitWindow(primaryStage);
 		});
+		primaryStage.show();
 	}
 
 	@Override
