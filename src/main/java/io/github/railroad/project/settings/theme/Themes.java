a738e105abd1051b2fae889935b749f118daf529
DaRealTurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>
2021-10-16 19:39:34 +0100

    Update Themes.java

--- src/main/java/io/github/railroad/project/settings/theme/Themes.java
+++ src/main/java/io/github/railroad/project/settings/theme/Themes.java
@@ -2,7 +2,7 @@ package io.github.railroad.project.settings.theme;
 
 import static io.github.railroad.utility.helper.ColorHelper.fxColourToAwt;
 
-import static javafx.scene.paint.Color.*;
+import javafx.scene.paint.Color;
 
 /**
  * A list of default themes
@@ -12,11 +12,11 @@ import static javafx.scene.paint.Color.*;
  */
 public class Themes {
 
-	public static final Theme DARK_THEME = new Theme("DarkMode").withButtonColor(fxColourToAwt(DARKGRAY))
-			.withBackgroundColor(fxColourToAwt(DIMGRAY))
-			.withTextColor(fxColourToAwt(ALICEBLUE));
+	public static final Theme DARK_THEME = new Theme("DarkMode").withButtonColor(fxColourToAwt(Color.DARKGRAY))
+			.withBackgroundColor(fxColourToAwt(Color.DIMGRAY))
+			.withTextColor(fxColourToAwt(Color.ALICEBLUE));
 
-	public static final Theme WHITE_THEME = new Theme("WhiteMode").withButtonColor(fxColourToAwt(ANTIQUEWHITE))
-			.withBackgroundColor(fxColourToAwt(FLORALWHITE))
-			.withTextColor(fxColourToAwt(BLACK));
+	public static final Theme WHITE_THEME = new Theme("WhiteMode").withButtonColor(fxColourToAwt(Color.ANTIQUEWHITE))
+			.withBackgroundColor(fxColourToAwt(Color.FLORALWHITE))
+			.withTextColor(fxColourToAwt(Color.BLACK));
 }
